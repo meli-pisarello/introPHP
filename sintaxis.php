455118d5791f70fd21fd4859d91b0d692245e0c0
meli <mepisarello@gmail.com>
2019-10-13 15:36:40 -0300

    Version 0.0.1

---
+++ sintaxis.php
@@ -0,0 +1,6 @@
+<?php
+// esto es un comentario
+
+print '<h1>Hola mundo con PHP</h1>';
+echo '<br>Aprendiendo PHP'. ': introducción al curso';
+?>
---
+++ variables.php
@@ -0,0 +1,37 @@
+<?php
+
+// Variable tipo numero 
+$numero = 10;
+
+echo 'Esto es un número: ' . $numero;
+echo "<br><br>";
+
+// Variable tipo texto
+$texto = "Esto es una cadena de texto";
+
+echo 'Mi cadena: ' . $texto;
+echo "<br><br>";
+
+// Variable de tipo booleana
+$booleana = true;
+// si es true muestra 1, si es falso no muestra nada
+echo "Esto es una variable booleana: " . $booleana;
+echo "<br><br>";
+
+
+// Variable de tipo arreglo
+$arreglo = array("Alejando", "Maria");
+echo "Segundo nombre del arreglo: " . $arreglo[1];
+echo "<br><br>";
+
+// Variable tipo arreglo con propiedades
+$colores = array("color1"=>"rojo", "color2"=>"azul");
+
+echo $colores["color2"];
+echo "<br><br>";
+
+// variable de tipo objecto
+$objeto = (object)["mueble1"=>"armario", "mueble2" => "escritorio"];
+
+echo $objeto->mueble2
+?>
